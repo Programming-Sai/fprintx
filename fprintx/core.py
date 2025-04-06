58dd7db440044ce28f2ad770738c72ff78719fae
Programming-Sai <saiahprog6@gmail.com>
2025-04-06 16:56:57 +0100

    Done working on core functionality. about to start testing it out.

---
+++ fprintx/core.py
@@ -0,0 +1,65 @@
+# fprintx/core.py
+
+def printx(*args, widths=None, alignments=None, styles=None, truncate=False, return_as_str=False):
+    # Default values if widths, alignments, or styles are not provided
+    min_arg_width = [min(len(str(arg)) for arg in args)][0]
+    if widths is None:
+        widths = [max(len(str(arg)) for arg in args)] * len(args)
+    
+    if alignments is None:
+        alignments = ['<'] * len(args)
+    
+    if styles is None:
+        styles = [''] * len(args)
+
+
+
+    # Ensure lengths match or apply last option to remaining values
+    widths = widths + ( [widths[-1]] * (max(0, len(args) - len(widths))))
+    alignments = alignments + ( [alignments[-1]] * (max(0, len(args) - len(alignments))))
+    styles = styles + ( [styles[-1]] * (max(0, len(args) - len(styles))))
+
+    # print(widths, alignments, styles, min_arg_width)
+
+    formatted_args = []
+    
+    for i, arg in enumerate(args):
+            
+        value = str(arg)
+        style = styles[i]
+        width = widths[i]
+        alignment = alignments[i]
+
+        if truncate and len(value) > width + 10:
+            value = value[:width] + '...'
+        elif truncate and len(value) > width:
+            value = value[:width]
+
+
+
+        # Apply style formatting (bold, underline, etc.)
+        if style == 'bold':
+            value = f"\033[1m{value}\033[0m"  + (" " * (width - 1))
+        elif style == 'underline':
+            value = f"\033[4m{value}\033[0m"  + (" " * (width - 1))
+        elif style == 'italic':
+            value = f"\033[3m{value}\033[0m" + (" " * (width - 1))
+        
+
+        # Apply alignment and width
+        formatted_value = f"{value:{alignment}{width}}"
+        formatted_args.append(formatted_value)
+    
+    # Print the formatted output
+    result = " ".join(formatted_args)
+    if return_as_str:
+        return result
+    print(result)
+
+
+
+
+# printx("HEllo", 'Kow', 353434353434353434353434353434353434353434353434353434, widths=[10], styles=['', 'italic'], truncate=True)
+# printx("HEllo", 'Kow', 353434353434353434353434353434353434353434353434353434, widths=[10], styles=['', 'italic'], truncate=True)
+# printx("HEllo", 'Kow', 2, widths=[10], styles=['', 'italic'], truncate=True)
+# printx("HEllo", 'Kow', 'lkl', widths=[10], truncate=True, styles=['bold'])
